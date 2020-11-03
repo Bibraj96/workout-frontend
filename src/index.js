1420e664c27231dbde801cace442bf4346e83abf
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-03 18:22:40 -0500

    import provider compose applymiddleware and thunk

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { thunk } from 'react-redux';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
