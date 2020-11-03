22efc424fcd9440e02c6f3bff3181a622b671f13
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-03 18:16:50 -0500

    convert into class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      React App
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        React App
+      </div>
+    );
+  }
 }
 
 export default App;
