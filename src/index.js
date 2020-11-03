282d14e255e90923a1372a54a37fad98c5126746
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-03 18:36:44 -0500

    import combinereducers and use to create mock reducer in order test store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { thunk } from 'react-redux';
-import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const users = () => [];
+
+const reducer = combineReducers({
+  users
+})
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
