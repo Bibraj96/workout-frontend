c4161558c46beba9e8ecc802bd60e27c645d1d53
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-03 18:26:28 -0500

    create composeEnhancer to allow use of redux tools

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { thunk } from 'react-redux';
 import { Provider } from 'react-redux';
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={ store }>
