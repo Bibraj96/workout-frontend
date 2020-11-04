2f3e537b7c11b52ddae15915897c0b55f2fd953e
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-04 12:54:25 -0500

    add mock async action creator to fetch current user

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,7 +1,20 @@
-export const setCurrentUser = user => {
+// synchronous action creators
+export const setCurrentUser = user => { // we set the user in the argument equal to the user in the store
   return {
     type: 'SET_CURRENT_USER',
     user // again, this is just the truncated syntax of user: user
   }
 }
 
+// asynchronous action creators
+export const login = credentials => {
+  return dispatch => {
+    return fetch("http:localhost:3000/api/v1/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({username: "blendi", password: "password"})
+    })
+  }
+}
