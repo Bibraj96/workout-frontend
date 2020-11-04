1b4964f502581726e58b87f34ac275fe366a8c55
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-04 13:52:39 -0500

    add placeholders

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <form onSubmit={}>
-      <input type="text" name="username" value={}  onChange={}/>
-      <input type="text" name="password" value={}  onChange={}/>
+    <form onSubmit={undefined}>
+      <input placeholder="username" type="text" name="username" value={undefined}  onChange={undefined}/>
+      <input placeholder="password" type="text" name="password" value={undefined}  onChange={undefined}/>
       <input type="submit" value="Log In"/>
     </form>
   )
