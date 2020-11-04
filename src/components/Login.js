2ee3250a436bbb9bdc94b5d663ee752fae3e7f1a
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-04 14:14:24 -0500

    import connect and create mapStateToProps function to pass into connect

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux'
 
-const Login = () => {
+const Login = ({username, password}) => {
   return (
     <form onSubmit={undefined}>
-      <input placeholder="username" type="text" name="username" value={undefined}  onChange={undefined}/>
-      <input placeholder="password" type="text" name="password" value={undefined}  onChange={undefined}/>
+      <input placeholder="username" type="text" name="username" value={username}  onChange={undefined}/>
+      <input placeholder="password" type="text" name="password" value={password}  onChange={undefined}/>
       <input type="submit" value="Log In"/>
     </form>
   )
 }
 
-export default Login
+// This gives us access to these chunks of state from the store as props
+// We pass these props into the argument of Login on line 4
+// {props: {username: "blendi", password: "password"}}
+// You can either use props.username, or use destructuring
+const mapStateToProps = state => {
+  return {
+    username: state.loginForm.username,
+    password: state.loginForm.password
+  }
+}
+
+export default connect(mapStateToProps)(Login)
