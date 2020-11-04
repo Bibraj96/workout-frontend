23c03d542ed5e1b8c8604da3dff95e35415037cb
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-04 13:41:33 -0500

    add login form

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <form onSubmit={}>
+      <input type="text" name="username" value={}  onChange={}/>
+      <input type="text" name="password" value={}  onChange={}/>
+      <input type="submit" value="Log In"/>
+    </form>
+  )
+}
+
+export default Login
