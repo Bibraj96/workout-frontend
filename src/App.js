1fd4e9bc47123886cdcfc1ae104ac36883ed8056
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-04 13:52:02 -0500

    render Login component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
+import Login from './components/Login.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        React App
-      </div>
+      <Login />
     );
   }
 }
