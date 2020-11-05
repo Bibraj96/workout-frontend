8c134da49f8907a8dfa6b949579b3b31c38e511e
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-05 18:22:52 -0500

    Create conditional for logout functionality and add mapStateToProps to give App access to current User

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Login from './components/Login.js'
+import Logout from './components/Logout.js'
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser.js'
 
@@ -11,9 +12,18 @@ class App extends Component {
 
   render() {
     return (
-      <Login />
+      this.props.currentUser ? <Logout /> : <Login />
     );
   }
 }
 
-export default connect(null, {getCurrentUser})(App);
+// This will make the current user available to app to use
+// We can destructure currentUser out of our state and use the shorthand currentUser instead of currentUser: currentUser
+// Otherwise, state would be our arguement and we'd assign our key like this: currentUser: state.currentUser
+const mapStateToProps = ({ currentUser }) => {
+  return {
+    currentUser
+  }
+}
+
+export default connect(mapStateToProps, {getCurrentUser})(App);
