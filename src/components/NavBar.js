cd82a75148553521e619f1b8756796a0d4e347f4
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-05 19:14:27 -0500

    remove mapStateToProps and comments from App to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,6 +8,10 @@ const NavBar = ({ logout }) => {
   )
 }
 
+// This will make the current user available to app to use
+// We can destructure currentUser out of our state and use the shorthand currentUser instead of currentUser: currentUser
+// Otherwise, state would be our arguement and we'd assign our key like this: currentUser: state.currentUser
+
 const mapStateToProps = ({ currentUser }) => {
   return {
     currentUser
