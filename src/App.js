300bb0b3863230d41bcc81c98b01f062ccaa7bb3
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-05 19:14:17 -0500

    remove mapStateToProps and comments from App to NavBar

--- src/App.js
+++ src/App.js
@@ -18,13 +18,4 @@ class App extends Component {
   }
 }
 
-// This will make the current user available to app to use
-// We can destructure currentUser out of our state and use the shorthand currentUser instead of currentUser: currentUser
-// Otherwise, state would be our arguement and we'd assign our key like this: currentUser: state.currentUser
-const mapStateToProps = ({ currentUser }) => {
-  return {
-    currentUser
-  }
-}
-
-export default connect(mapStateToProps, {getCurrentUser})(App);
+export default connect(null, {getCurrentUser})(App);
