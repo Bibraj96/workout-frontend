017ba7ea0851fc8c51f2cc57231761833cab0a93
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-08 17:10:45 -0500

    create action creator to fetch all of the workouts

--- src/actions/myWorkouts.js
+++ src/actions/myWorkouts.js
@@ -1,6 +1,30 @@
+// synchronous action creators
 export const setMyWorkouts = workouts => {
   return {
     type: 'SET_MY_WORKOUTS',
     workouts
+  }
+}
+
+// asynchronous action creators
+export const getMyWorkouts = () =>  {
+  return dispatch => {
+    return fetch("http://localhost:3000/api/v1/workouts", {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+      .then(r => r.json())
+      .then(workouts => {
+        if (workouts.error) {
+          alert(workouts.error)
+        } else {
+          console.log(workouts)
+          dispatch(setMyWorkouts([workouts]))
+        }
+      })
+      .catch(console.log)
   }
 }
