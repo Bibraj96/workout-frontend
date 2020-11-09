68a4f32e6daf1277b9c473925b0cd680730f5fc5
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-09 17:13:32 -0500

    add mapStateToProps to access LoggedIn state and create a route to Home if logged in

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser.js'
 import NavBar from './components/NavBar.js'
 import MainContainer from './components/MainContainer.js'
+import Home from './components/Home.js'
 import Login from './components/Login.js'
 import Logout from './components/Logout.js'
 import Signup from './components/Signup.js'
 import MyWorkouts from './components/MyWorkouts.js'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 class App extends Component {
 
@@ -17,19 +18,28 @@ class App extends Component {
   }
 
   render() {
+    const { loggedIn } = this.props
     return (
       <Router>
         <div className="App">
-          <Logout />
           <NavBar />
           <MainContainer />
+          <Switch>
             <Route exact path='/signup' component={Signup}/>
             <Route exact path='/login' component={Login}/>
+            <Route exact path='/' render={() => loggedIn ? <MyWorkouts /> : <Home />}/>
             <Route exact path='/my-workouts' component={MyWorkouts}/>
+          </Switch>
         </div>
       </Router>
     );
   }
 }
 
-export default connect(null, {getCurrentUser})(App);
+const mapStateToProps = state => {
+  return ({
+    loggedIn: !!state.currentUser
+  })
+}
+
+export default connect(mapStateToProps, {getCurrentUser})(App);
