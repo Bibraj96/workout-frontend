93a406990784279cc8f3c732816fb113501e846b
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-09 12:27:55 -0500

    import Signup component and create a route for it

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { getCurrentUser } from './actions/currentUser.js'
 import NavBar from './components/NavBar.js'
 import MainContainer from './components/MainContainer.js'
 import Login from './components/Login.js'
-import Logout from './components/Logout.js'
+import Signup from './components/Signup.js'
 import MyWorkouts from './components/MyWorkouts.js'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
@@ -21,6 +21,7 @@ class App extends Component {
         <div className="App">
           <NavBar />
           <MainContainer />
+            <Route exact path='/signup' component={Signup}/>
             <Route exact path='/login' component={Login}/>
             <Route exact path='/my-workouts' component={MyWorkouts}/>
         </div>
