e5da5c097d725e4a43a1ee038812bb38352793a2
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-09 11:27:43 -0500

    fix import syntax and nest entire App div in Router tags

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser.js'
 import NavBar from './components/NavBar.js'
 import MainContainer from './components/MainContainer.js'
-import Login from './components/Login/js'
-import Logout from './components/Logout/js'
-import MyWorkouts from './components/MyWorkouts/js'
+import Login from './components/Login.js'
+import Logout from './components/Logout.js'
+import MyWorkouts from './components/MyWorkouts.js'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends Component {
@@ -17,15 +17,15 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <NavBar />
-        <MainContainer />
-        <Router>
-          <Route exact path='/login' component={Login}/>
-          <Route exact path='/logout' component={Logout}/>
-          <Route exact path='/my-workouts' component={MyWorkouts}/>
-        </Router>
-      </div>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <MainContainer />
+            <Route exact path='/login' component={Login}/>
+            <Route exact path='/logout' component={Logout}/>
+            <Route exact path='/my-workouts' component={MyWorkouts}/>
+        </div>
+      </Router>
     );
   }
 }
