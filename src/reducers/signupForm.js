1680b131a744f74557cc2d8eeb22668e9648f995
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-09 14:07:58 -0500

    arbitrary edits

--- src/reducers/signupForm.js
+++ src/reducers/signupForm.js
@@ -4,12 +4,12 @@ const initialState = {
   password: ""
 }
 
-export default (state = initialState, action) => {
-  switch(action.type) {
+export default (state=initialState, action) => {
+  switch (action.type) {
     case "UPDATE_SIGNUP_FORM":
       return action.formData
     case "RESET_SIGNUP_FORM":
-      return initialState;
+      return initialState
     default:
       return state
   }
