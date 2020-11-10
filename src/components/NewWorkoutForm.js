76703b8e575c7a5e5d4daa4780fc22b06fca266f
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-10 12:24:31 -0500

    create form to add a new workout

---
+++ src/components/NewWorkoutForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NewWorkoutForm = () => (
+  <form>
+    <input 
+    name="title"
+    onChange={}
+    value={}
+    />
+    <input 
+    name="date"
+    onChange={}
+    value={}
+    />
+  </form>
+)
+
+export default NewWorkoutForm;
