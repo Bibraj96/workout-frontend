ad33913a02a9b373747077976ac0a7fb50faed13
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-10 13:15:58 -0500

    add handleChange fn and import connect and updateNewWorkoutForm action

--- src/components/NewWorkoutForm.js
+++ src/components/NewWorkoutForm.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import { updateNewWorkoutForm } from '../actions/newWorkoutForm.js';
+import { connect } from 'react-redux'
 
-const NewWorkoutForm = () => (
-  <form>
-    <input 
-    name="title"
-    onChange={}
-    value={}
-    />
-    <input 
-    name="date"
-    onChange={}
-    value={}
-    />
-  </form>
-)
+const NewWorkoutForm = () => {
 
-export default NewWorkoutForm;
+  const handleChange = event => {
+    event.preventDefault()
+    const { name, value } = event.target
+    updateNewWorkoutForm(name, value)
+  }
+
+  return (
+    <form>
+      <input
+      placeholder="Title" 
+      name="title"
+      onChange={handleChange}
+      value={""}
+      /><br/>
+      <input
+      placeholder="Date" 
+      name="date"
+      onChange={handleChange}
+      value={""}
+      /><br/>
+      <input 
+      type="submit" 
+      value="Create Workout"/>
+    </form>
+)}
+
+export default connect()(NewWorkoutForm);
