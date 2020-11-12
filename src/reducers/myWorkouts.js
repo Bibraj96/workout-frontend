15e311907154fd21e9cf2644dca54e5ca172d2a9
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-12 10:43:14 -0500

    add clear workouts case to myWorkouts reducer

--- src/reducers/myWorkouts.js
+++ src/reducers/myWorkouts.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
   switch (action.type) {
     case "SET_MY_WORKOUTS":
       return action.workouts
+    case "CLEAR_WORKOUTS":
+      return []
     default:
       return state
   }
