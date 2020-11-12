7d483d6a3541b255c06e3b7d445284d3f5285385
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-12 16:29:47 -0500

    add createWorkout action with a post fetch request

--- src/actions/myWorkouts.js
+++ src/actions/myWorkouts.js
@@ -41,3 +41,24 @@ export const getMyWorkouts = () =>  {
       .catch(console.log)
   }
 }
+
+export const createWorkout = workoutData => {
+  return dispatch => {
+    const snakeWorkoutData = { // Only needed this for user_id, but did it for the others to keep consistent
+      title: workoutData.title,
+      date: workoutData.date,
+      user_id: workoutData.userId
+    }
+    return fetch("http://localhost:3000/api/v1/workouts", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(snakeWorkoutData)
+    })
+    .then(res => res.json())
+    .then(console.log)
+    .catch(console.log)
+  }
+}
