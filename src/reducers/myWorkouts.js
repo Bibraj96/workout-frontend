242ad37952fa277f3795f057ad38c987de0fa8e7
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-12 14:53:52 -0500

    concat workout to myWorkouts when add trip action is fired

--- src/reducers/myWorkouts.js
+++ src/reducers/myWorkouts.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
   switch (action.type) {
     case "SET_MY_WORKOUTS":
       return action.workouts
+    case "ADD_WORKOUT":
+      return state.workouts.concat(action.workouts)
     case "CLEAR_WORKOUTS":
       return []
     default:
