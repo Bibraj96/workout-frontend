b4311b0b2756770225d9190572e4c7b58de9c422
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-12 18:52:21 -0500

    create conditional to render each workout

--- src/components/WorkoutCard.js
+++ src/components/WorkoutCard.js
@@ -3,10 +3,12 @@ import React from 'react'
 const WorkoutCard = ({ workout }) => {
   return (
   // you could also use fragments instead of a div here too: <> </>
+  workout ? 
   <div>
     <h4>{workout.title}</h4>
     <p>{workout.date}</p>
-  </div>
+  </div> :
+  <p>Workout Card</p>
   )
 }
 
