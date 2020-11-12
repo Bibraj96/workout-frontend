17c005b3c1f90916ae29cdc38eca099f12529bfb
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-12 10:41:46 -0500

    redirect to home page after logout

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { logout } from '../actions/currentUser.js'
+import { withRouter } from 'react-router-dom'
 
-const Logout = ({ logout }) => {
+const Logout = ({ logout, history }) => {
 
   return (
-    <form onSubmit={logout}>
+    <form onSubmit={(event) => {
+        event.preventDefault() // Prevent refresh
+        logout() // Fire logout action
+        history.push('/') // Go back to home page after logout
+      }
+    }>
       <input type="submit" value="Log Out"/>
     </form>
   )
 }
 
-export default connect(null, { logout })(Logout)
-
-// need:
-// action creator
-// case statement in currentuser reducer
+export default withRouter(connect(null, { logout })(Logout))
