86b1b56e36d7b0ecde0057a07dba9419c51bc013
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-13 16:51:03 -0500

    convert to class component in order to user componentDidMount to populate the form fields

--- src/components/EditWorkoutFormWrapper.js
+++ src/components/EditWorkoutFormWrapper.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import { createWorkout } from '../actions/myWorkouts'
+import { updateWorkout } from '../actions/myWorkouts'
+import { setEditWorkoutForm } from '../actions/workoutForm'
 import WorkoutForm from './WorkoutForm.js'
 import { connect } from 'react-redux'
+// We need to make this a class component so that we can populate the edit form fields using a componentDidMount
 
-const NewWorkoutFormWrapper = ({history, createWorkout}) => {
+class EditWorkoutFormWrapper extends React.Component {
 
-  const handleSubmit = (event, formData, userId, history) => {
+  componentDidMount() {
+    this.props.workout && this.props.setEditWorkoutForm(this.props.workout)
+  }
+
+  handleSubmit = (event, formData, userId, history) => {
+    const { updateWorkout, workout } = this.props
     event.preventDefault()
     console.log("Got to WorkoutFormWrapper")
-    createWorkout({
+    updateWorkout({
       ...formData,
+      workoutId: workout.id,
       userId
     }, history)
   }
   
-  return <WorkoutForm history={history} handleSubmit={handleSubmit} />
+  render() {
+    const { handleSubmit, history } = this.props;
+    return <WorkoutForm editMode history={history} handleSubmit={handleSubmit} />
+  }
 }
 
-export default connect(null, { createWorkout })(NewWorkoutFormWrapper);
+export default connect(null, { updateWorkout, setEditWorkoutForm })(EditWorkoutFormWrapper);
