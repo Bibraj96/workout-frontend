8054f5be1b843fa52b055b87a84996d7a324cedd
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-13 16:51:57 -0500

    add update workout case to reducer

--- src/reducers/myWorkouts.js
+++ src/reducers/myWorkouts.js
@@ -4,6 +4,9 @@ export default (state = [], action) => {
       return action.workouts
     case "ADD_WORKOUT":
       return state.concat(action.workout)
+    case "UPDATE_WORKOUT":
+      console.log("in update workout")
+      return state
     case "CLEAR_WORKOUTS":
       return []
     default:
