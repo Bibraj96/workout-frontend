fa41eb29622dd5bfa50230e8d2826ec3e034485e
bibraj96 <blendi.ibraj96@gmail.com>
2020-11-13 16:02:58 -0500

    add action to set the edit workout form

--- src/actions/workoutForm.js
+++ src/actions/workoutForm.js
@@ -12,3 +12,14 @@ export const resetNewWorkoutForm = () => {
     type: "RESET_NEW_WORKOUT_FORM"
   }
 }
+
+export const setEditWorkoutForm = workout => {
+  const workoutFormData = { // Need to explicitly inform redux to populate the form fields with this 
+    title: workout.title,
+    date: workout.date
+  }
+  return {
+    type: "SET_EDIT_FORM_DATA",
+    workoutFormData
+  }
+}
